94d7589d1a24c3cb04966e07efb40a9ce7dd730f
Finn Reilly <finn.reilly@mountainwarehouse.com>
2025-05-06 00:07:01 +0100

    First attempt at service registration

---
+++ Aspector.Core/Attributes/AspectAttribute.cs
@@ -0,0 +1,10 @@
+﻿using Castle.DynamicProxy;
+
+namespace Aspector.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Method)]
+    public abstract class AspectAttribute : Attribute, IInterceptor
+    {
+        public abstract void Intercept(IInvocation invocation);
+    }
+}
---
+++ Aspector.Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Aspector.Core.Attributes;
+using Castle.DynamicProxy;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Aspector.Core.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAspects(this IServiceCollection services)
+        {
+            services.AddSingleton(new ProxyGenerator());
+
+            var decoratedTypes = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(
+                    t =>
+                        t.GetMembers(BindingFlags.Public | BindingFlags.Instance)
+                            .Any(m => m.GetCustomAttributes().Any(attr => attr.GetType().IsAssignableTo(typeof(AspectAttribute)))))
+                .ToHashSet();
+
+            var applicableServicesInContainer = services.Where(
+                descriptor => descriptor.ImplementationInstance != null 
+                    && descriptor.ServiceType.IsInterface
+                    && decoratedTypes.Contains(descriptor.ImplementationType!));
+
+            foreach(var serviceDescriptor in applicableServicesInContainer)
+            {
+                var applicableAspects = serviceDescriptor
+                    .ImplementationType!
+                    .GetMembers(BindingFlags.Public | BindingFlags.Instance)
+                    .SelectMany(m => m.CustomAttributes.Where(a => a.AttributeType.IsAssignableTo(typeof(AspectAttribute))));
+
+                services.Remove(serviceDescriptor);
+
+                services.Add(new ServiceDescriptor(
+                    serviceDescriptor.ServiceType,
+                    factory: (provider) =>
+                    {
+                        var generator = provider.GetRequiredService<ProxyGenerator>();
+
+                        return generator.CreateInterfaceProxyWithTarget(serviceDescriptor.ServiceType, applicableAspects);
+                    },
+                    serviceDescriptor.Lifetime));
+            }
+            
+            return services;
+        }
+    }
+}
