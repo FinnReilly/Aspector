e5119d8741952a7743ce8cac9cb9102980b20a21
Finn Reilly <finn.reilly@mountainwarehouse.com>
2025-05-18 17:45:57 +0100

    alter models, as these will need to work on a per-class basis when wrapping proxy layers

--- Aspector.Core/Models/AspectAttributeSummary.cs
+++ Aspector.Core/Models/AspectAttributeSummary.cs
@@ -1,53 +1,66 @@
 ﻿using Aspector.Core.Attributes;
+using System.Reflection;
 
 namespace Aspector.Core.Models
 {
     public class AspectAttributeSummary
     {
-        public AspectAttributeSummary(AspectAttribute[] inputs)
+        public AspectAttributeSummary((MethodInfo method, AspectAttribute[] aspects)[] inputs)
         {
             Type? lastType = null;
-            for (var d = inputs.Length - 1; d >= 0; d--)
+            var maxDepthByMethod = new Dictionary<MethodInfo, int>();
+            foreach (var input in inputs)
             {
-                var targetAttribute = inputs[d];
-                var targetAttributeType = targetAttribute.GetType();
-                if (lastType != targetAttributeType)
-                {
-                    // mark new layer
-                    lastType = targetAttributeType;
-                    var thisLayerIndex = 0;
-                    if (MaximumIndexByType.TryGetValue(targetAttributeType, out var previousLayerIndex))
-                    {
-                        thisLayerIndex = previousLayerIndex + 1;
-                    }
+                var (decoratedMethod, aspectAttributes) = input;
+                var theseLayersFromInnermost = new List<AspectAttributeLayer>();
+                LayersFromInnermostByMethod[decoratedMethod] = theseLayersFromInnermost;
 
-                    MaximumIndexByType[targetAttributeType] = thisLayerIndex;
-                    LayersFromInnermost.Add(new AspectAttributeLayer(thisLayerIndex, targetAttribute));
-                }
-                else
+                for (var d = aspectAttributes.Length - 1; d >= 0; d--)
                 {
-                    LayersFromInnermost.Last().Add(targetAttribute);
+                    var targetAttribute = aspectAttributes[d];
+                    var targetAttributeType = targetAttribute.GetType();
+                    if (lastType != targetAttributeType)
+                    {
+                        // mark new layer
+                        lastType = targetAttributeType;
+                        var thisLayerIndex = 0;
+                        if (MaximumIndexByType.TryGetValue(targetAttributeType, out var previousLayerIndex))
+                        {
+                            thisLayerIndex = previousLayerIndex + 1;
+                        }
+
+                        MaximumIndexByType[targetAttributeType] = thisLayerIndex;
+                        theseLayersFromInnermost.Add(new AspectAttributeLayer(thisLayerIndex, targetAttribute));
+                    }
+                    else
+                    {
+                        theseLayersFromInnermost.Last().Add(targetAttribute);
+                    }
                 }
+
+                maxDepthByMethod[decoratedMethod] = theseLayersFromInnermost.Count;
             }
 
-            LayersByType = LayersFromInnermost.Aggregate(
-                seed: new Dictionary<Type, Dictionary<int, AspectAttributeLayer>>(),
-                func: (layersByType, singleLayer) => 
-                {
-                    if (!layersByType.TryGetValue(singleLayer.AspectType, out var layersForType))
-                    {
-                        layersForType = new Dictionary<int, AspectAttributeLayer>();
-                        layersByType[singleLayer.AspectType] = layersForType;
-                    }
+            //LayersByType = LayersFromInnermostByMethod.Aggregate(
+            //    seed: new Dictionary<MethodInfo, Dictionary<Type, Dictionary<int, AspectAttributeLayer>>>(),
+            //    func: (layersByType, singleLayer) => 
+            //    {
+            //        if (!layersByType.TryGetValue())
+            //        if (!layersByType.TryGetValue(singleLayer.AspectType, out var layersForType))
+            //        {
+            //            layersForType = new Dictionary<int, AspectAttributeLayer>();
+            //            layersByType[singleLayer.AspectType] = layersForType;
+            //        }
 
-                    layersForType[singleLayer.LayerIndex] = AspectAttributeLayer.FromReversed(singleLayer);
+            //        layersForType[singleLayer.LayerIndex] = AspectAttributeLayer.FromReversed(singleLayer);
 
-                    return layersByType;
-                });
+            //        return layersByType;
+            //    });
         }
 
-        Dictionary<Type, int> MaximumIndexByType = new Dictionary<Type, int>();
-        List<AspectAttributeLayer> LayersFromInnermost { get; } = new List<AspectAttributeLayer>();
-        Dictionary<Type, Dictionary<int, AspectAttributeLayer>> LayersByType { get; } = new Dictionary<Type, Dictionary<int, AspectAttributeLayer>>();
+        public Dictionary<Type, int> MaximumIndexByType = new Dictionary<Type, int>();
+        public List<(Type AspectType, int LayerIndex)> WrapOrder = new List<(Type AspectType, int LayerIndex)>();
+        public Dictionary<MethodInfo, List<AspectAttributeLayer>> LayersFromInnermostByMethod { get; } = new Dictionary<MethodInfo, List<AspectAttributeLayer>>();
+        public Dictionary<MethodInfo, Dictionary<Type, Dictionary<int, AspectAttributeLayer>>> LayersByType { get; } = new Dictionary<MethodInfo, Dictionary<Type, Dictionary<int, AspectAttributeLayer>>>();
     }
 }
