1429046e40052299980dbf0e37e6eb6b06d74e15
Finn Reilly <finn.reilly@mountainwarehouse.com>
2025-05-24 14:13:07 +0100

    improve XML docs for Access Control attributes

--- Aspector.Core/Attributes/AccessControl/AuthoriseObjectAccessAsyncAttribute.cs
+++ Aspector.Core/Attributes/AccessControl/AuthoriseObjectAccessAsyncAttribute.cs
@@ -1,5 +1,13 @@
-﻿namespace Aspector.Core.Attributes.AccessControl
+﻿using Aspector.Core.AccessControl;
+
+namespace Aspector.Core.Attributes.AccessControl
 {
+    /// <summary>
+    /// Triggers the use of an implementation of <see cref="AuthoriseObjectAccessAsyncDecorator{TResult}"/>.  This
+    /// is an abstract base class that requires a specific implementation to be written based on domain-specific authorisation
+    /// logic.
+    /// </summary>
+    /// <typeparam name="TResult">The object type for which object level authorisation is being carried out</typeparam>
     public class AuthoriseObjectAccessAsyncAttribute<TResult> : AuthoriseObjectAccessAttribute<TResult>
     {
     }
