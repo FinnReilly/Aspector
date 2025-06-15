373e0dc952a14a278bed829761f62b17002e9ea1
Finn Reilly <finn.reilly@mountainwarehouse.com>
2025-06-15 22:16:14 +0100

    config and packaged for EF core in Examples project

--- Aspector.Examples/Program.cs
+++ Aspector.Examples/Program.cs
@@ -1,15 +1,27 @@
 using Aspector.Core.Extensions;
+using Aspector.Examples.Data;
 using Aspector.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<IWeatherService, WeatherService>();
+builder.Services.AddDbContext<ExampleContext>(
+    opt => 
+        opt.UseSqlite(new SqliteConnection("Filename=:memory:")));
 
 builder.Services.AddAspects();
 
 var app = builder.Build();
+
+// migrations
+using (var serviceScope = app.Services.CreateScope())
+{
+    await serviceScope.ServiceProvider.GetRequiredService<ExampleContext>().Database.MigrateAsync();
+}
 
 // Configure the HTTP request pipeline.
 
