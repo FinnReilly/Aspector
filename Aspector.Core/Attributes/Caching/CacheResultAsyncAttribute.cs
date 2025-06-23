25275c626b50e567ab05be8817a426419c43ea5d
Finn Reilly <finn.reilly@mountainwarehouse.com>
2025-06-23 21:09:15 +0100

    XML docs for Caching attributes

--- Aspector.Core/Attributes/Caching/CacheResultAsyncAttribute.cs
+++ Aspector.Core/Attributes/Caching/CacheResultAsyncAttribute.cs
@@ -1,23 +1,31 @@
 ﻿namespace Aspector.Core.Attributes.Caching
 {
-    ///</inheritdoc>
+    /// <summary>
+    /// Caches the result of a async method according to configured parameters
+    /// </summary>
+    /// <typeparam name="TResult">The type of result returned by an awaitable <see cref="Task{TResult}"/> returned by the decorated method</typeparam>
+    
     public class CacheResultAsyncAttribute<TResult> : CacheResultAttribute<TResult>
     {
+        /// <inheritdoc/>
         public CacheResultAsyncAttribute(double timeToCacheSeconds) 
             : base(timeToCacheSeconds)
         {
         }
 
+        /// <inheritdoc/>
         public CacheResultAsyncAttribute(double timeToCacheSeconds, string cacheKeyParameter)
             : base(timeToCacheSeconds, cacheKeyParameter)
         {
         }
 
+        /// <inheritdoc/>
         public CacheResultAsyncAttribute(double timeToCacheSeconds, bool slidingExpiration) 
             : base(timeToCacheSeconds, slidingExpiration)
         {
         }
 
+        /// <inheritdoc/>
         public CacheResultAsyncAttribute(double timeToCacheSeconds, bool slidingExpiration, string cacheKeyParameter) 
             : base(timeToCacheSeconds, slidingExpiration, cacheKeyParameter)
         { 
--- Aspector.Core/Attributes/Caching/CacheResultAttribute.cs
+++ Aspector.Core/Attributes/Caching/CacheResultAttribute.cs
@@ -1,5 +1,9 @@
 ﻿namespace Aspector.Core.Attributes.Caching
 {
+    /// <summary>
+    /// Caches the result of a method according to configured parameters
+    /// </summary>
+    /// <typeparam name="TResult">The type of result returned by the decorated method</typeparam>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     public class CacheResultAttribute<TResult> : AspectAttribute
     {
@@ -7,7 +11,10 @@
 
         public int? TimeToCacheMilliseconds { get; }
         public bool SlidingExpiration { get; }
+
         /// <summary>
+        /// Gets and sets a single hardcoded cache key for all results
+        /// <br/><br/>
         /// Attention - setting this will override any cache key parameter
         /// </summary>
         public object? CacheKey
@@ -19,31 +26,45 @@
                 _cacheKey = value!;
             }
         }
+
+        /// <summary>
+        /// Gets or sets an enum describing how to behave if a parameter to be used for a cache key is null.
+        /// <br/><br/>
+        /// The default is <see cref="NullCacheKeyBehaviour.Throw"/>
+        /// </summary>
         public NullCacheKeyBehaviour NullCacheKeyBehaviour { get; set; } = NullCacheKeyBehaviour.Throw;
         public string? CacheKeyParameter { get; private set; } = null;
         public Type Type { get; } = typeof(TResult);
 
+        /// <param name="timeToCacheSeconds">The amount of time to cache the result in seconds</param>
         public CacheResultAttribute(double timeToCacheSeconds)
         {
             TimeToCacheMilliseconds = (int)Math.Floor(timeToCacheSeconds * 1000);
         }
 
-        public CacheResultAttribute(double timeToCacheSeconds, string keyParameter)
+        /// <param name="timeToCacheSeconds">The amount of time to cache the result in seconds</param>
+        /// <param name="cacheKeyParameter">The name of a parameter whose value will be used as a cache key</param>
+        public CacheResultAttribute(double timeToCacheSeconds, string cacheKeyParameter)
             : this(timeToCacheSeconds)
         {
-            CacheKeyParameter = keyParameter;
+            CacheKeyParameter = cacheKeyParameter;
         }
 
+        /// <param name="timeToCacheSeconds">The amount of time to cache the result in seconds</param>
+        /// <param name="slidingExpiration">Whether to use sliding expiration</param>
         public CacheResultAttribute(double timeToCacheSeconds, bool slidingExpiration)
             : this(timeToCacheSeconds)
         {
             SlidingExpiration = slidingExpiration;
         }
 
-        public CacheResultAttribute(double timeToCacheSeconds, bool slidingExpiration, string keyParameter)
+        /// <param name="timeToCacheSeconds">The amount of time to cache the result in seconds</param>
+        /// <param name="slidingExpiration">Whether to use sliding expiration</param>
+        /// <param name="cacheKeyParameter">The name of a parameter whose value will be used as a cache key</param>
+        public CacheResultAttribute(double timeToCacheSeconds, bool slidingExpiration, string cacheKeyParameter)
             : this(timeToCacheSeconds, slidingExpiration)
         {
-            CacheKeyParameter = keyParameter;
+            CacheKeyParameter = cacheKeyParameter;
         }
     }
 }
